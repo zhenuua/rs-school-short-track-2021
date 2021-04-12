6b5acc731b48de6792812cac810ca4e357d7f9fd
zhenuua <zhenuua@gmail.com>
2021-04-12 12:49:04 +0300

    feat: do part of task 10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,22 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  if (!Array.isArray(array)) {
+    throw new Error('Not implemented');
+  }
+  if (array.indexOf(value) === -1) {
+    return false;
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (!(typeof array[i] === 'number')) {
+      return false;
+    }
+  }
+
+  array.sort((a, b) => a - b);
+
+  return array.indexOf(value);
 }
 
 module.exports = findIndex;
