caf3bb18dfaaace254877da5f8be8413bcc35be2
zhenuua <zhenuua@gmail.com>
2021-04-12 23:28:48 +0300

    feat: do task number 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
-}
+function encodeLine(str) {
+  if (!(typeof str === 'string')) {
+    throw new Error('Not implemented');
+  }
 
+  let count = 1;
+  const arr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      if (count > 1) {
+        arr.push(`${count}${str[i]}`);
+      } else {
+        arr.push(`${str[i]}`);
+      }
+      count = 1;
+    }
+  }
+  return arr.join('');
+}
 module.exports = encodeLine;
